208d64f68378ed41dc306a16c053d269cad86b85
Cristian Prieto <me@cprieto.com>
2017-06-23 14:26:39 +1000

    Delay response when requested

--- whoami/app.py
+++ whoami/app.py
@@ -1,11 +1,35 @@
-from flask import Flask
-import socket
+from flask import Flask, make_response
+import socket, time, logging, sys
 
 app = Flask(__name__)
+Fail = False
+hostname = socket.gethostname()
 
 @app.route("/")
 def hello():
     return "Host: {host}".format(host=socket.gethostname())
 
+@app.route("/health")
+def health():
+    if Fail:
+        app.logger.warning("Delaying response to %s", hostname)
+        time.sleep(7)
+        
+    return ""
+
+@app.route("/delay")
+def fail():
+    global Fail
+    Fail = True
+    app.logger.warning('Next request to %s will be delayed', hostname)
+    return "next request to http://{hostname}/health will be delayed 7 seconds".format(hostname=hostname)
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+    handler = logging.StreamHandler(sys.stdout)
+    handler.setLevel(logging.INFO)
+    app.logger.addHandler(handler)
+
+    try:
+        app.run(host='0.0.0.0', port=80)
+    except KeyboardInterrupt:
+        app.stop()
